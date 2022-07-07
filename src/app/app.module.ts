61617fe59efea718146c86e473c78a850b4c2d9d
Polina Boretc <101650145+Polinaboretc@users.noreply.github.com>
2022-07-07 13:26:30 +0200

    final

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { HttpClientModule } from '@angular/common/http'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MeteoForecastComponent } from './components/meteo-forecast/meteo-forecast.component';
-import { CodePipe } from './code.pipe';
+import { CodePipe } from './pipes/code.pipe';
+import { UnitPipe } from './pipes/unit.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
     MeteoForecastComponent,
-    CodePipe
+    CodePipe,
+    UnitPipe
   ],
   imports: [
     BrowserModule,
--- src/app/pipes/code.pipe.ts
+++ src/app/pipes/code.pipe.ts
@@ -7,11 +7,16 @@ export class CodePipe implements PipeTransform {
 
   transform(value: number, ...args: any[]): any {
     switch (value) {
-      case 1||2||3:
+      case 1:
+      case 2:
+      case 3:
         return "../../../assets/cloud-sun-solid.svg"
-      case 80||81||82:
+      case 80:
+      case 81:
+      case 82:
         return "../../../assets/cloud-showers-heavy-solid.svg"
-      case 45||48:
+      case 45:
+      case 48:
         return "../../../assets/smog-solid.svg"
       default:0
       return "../../../assets/sun-solid.svg"
